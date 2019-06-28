c01f783c5af1084daec7870f53d9d0eb7ed51845
Xing-Yu Zhou <xing-yu.zhou@desy.de>
2019-06-28 17:05:26 +0900

    Update src/countIncLiaInVlib.cpp

--- src/countIncLiaInVlib.cpp
+++ src/countIncLiaInVlib.cpp
@@ -12,22 +12,13 @@ unsigned int topoana::countIncLiaInVlib(list<int> & lia, vector< list<int> > & V
     }
 
   list<int>::iterator liita;
-  unsigned int ibegin;
   unsigned int iend;
   liita=lia.begin();
   // The following if statement is designed for the inclusive decays started directly from the initial e+ and e-.
-  if((*liita)==-11)
-    {
-      ibegin=0;
-      iend=1;
-    }
-  else
-    {
-      ibegin=1;
-      iend=Vlib.size();
-    }
+  if((*liita)==-11) iend=1;
+  else iend=Vlib.size();
 
-  for(unsigned int i=ibegin;i<iend;i++) if(isLiaMatchedWithLib(lia,Vlib[i])) nCount++;
+  for(unsigned int i=0;i<iend;i++) if(isLiaMatchedWithLib(lia,Vlib[i])) nCount++;
 
   return nCount;
 }
