0d85dded19e26257ecf15be1b2f5693e10d6db15
Xing-Yu Zhou <xing-yu.zhou@desy.de>
2020-03-10 03:01:38 +0900

    Update examples/test/test_cmd_line_args/jpsi_ta.sh mainly with the uses of wildcards

--- examples/test/test_cmd_line_args/jpsi_ta.sh
+++ examples/test/test_cmd_line_args/jpsi_ta.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 topoana.exe jpsi_ta.card -i ../../in_the_paper/input/jpsi1.root -o jpsi1_ta -n 25000
-topoana.exe jpsi_ta.card -i ../../in_the_paper/input/jpsi1.root ../../in_the_paper/input/jpsi2.root -o jpsi2_ta -n 75000
+topoana.exe jpsi_ta.card -i ../../in_the_paper/input/jpsi2.root -o jpsi2_ta -n 25000
+topoana.exe jpsi_ta.card -i ../../in_the_paper/input/jpsi1.root ../../in_the_paper/input/jpsi2.root -o jpsi3_ta -n 75000
+topoana.exe jpsi_ta.card -i ../../in_the_paper/input/jpsi[1-2].root -o jpsi4_ta -n 75000
+topoana.exe jpsi_ta.card -i ../../in_the_paper/input/jpsi?.root -o jpsi5_ta -n 75000
+topoana.exe jpsi_ta.card -i ../../in_the_paper/input/jpsi*.root -o jpsi6_ta -n 75000
 exit 0
