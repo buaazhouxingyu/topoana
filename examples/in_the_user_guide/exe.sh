3391e0e959ab424707011273babd1ee77c526672
Xing-Yu Zhou <xing-yu.zhou@desy.de>
2020-09-11 19:46:34 +0900

    Revise examples/in_the_paper/exe.sh and examples/in_the_user_guide/exe.sh

--- examples/in_the_user_guide/exe.sh
+++ examples/in_the_user_guide/exe.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-for((i=1;i<=26;i++))
+
+cd ex_for_tb_01
+   pwd
+   ls
+   ../../../bin/topoana.exe jpsi_ta.card 
+cd ../
+
+for((i=2;i<=27;i++))
 do
 num=`printf "%02d" $i`
 if [ -d ex_for_tb_$num ]
 then
 cd ex_for_tb_$num
-pwd
-ls
-../../../bin/topoana.exe ex_for_tb_$num.card
+  pwd
+  ls
+  ../../../bin/topoana.exe ex_for_tb_$num.card
 cd ../ 
 fi
 done
+
 exit 0
