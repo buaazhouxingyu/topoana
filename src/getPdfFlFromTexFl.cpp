e6d2191caadfe8ddf649291b888be0a2add856d5
Xing-Yu Zhou <xing-yu.zhou@desy.de>
2020-10-19 22:47:28 +0900

    Mainly add the missing '#include <fstream>' to src/getPdfFlFromTexFl.cpp

--- src/getPdfFlFromTexFl.cpp
+++ src/getPdfFlFromTexFl.cpp
@@ -1,4 +1,5 @@
 #include "../include/topoana.h"
+#include <fstream>
 #include <cstdlib>
 
 void topoana::getPdfFlFromTexFl()
@@ -6,6 +7,7 @@ void topoana::getPdfFlFromTexFl()
   if(m_anaTasksForSigIds=="T"&&m_vPid_compDcyBrP.size()==0&&m_vCompIncDcyBr.size()==0&&m_vCompIRADcyBr.size()==0&&m_compAnaOfDcyTrs==false&&m_compAnaOfDcyIFSts==false) return;
 
   string NmOfOptTexFl=m_comNmOfOptFls+".tex";
+
   //string pdflatexcmd="pdflatex "+NmOfOptTexFl;
   // The following eight lines of code is used to replace the statement above, in order to write the pdf file to the directory of the tex source file, in the cases where the tex source file is not in the current working directory.
   string optDir;
@@ -13,6 +15,7 @@ void topoana::getPdfFlFromTexFl()
   system(dirnamecmd.c_str());
   ifstream fin("dirname.txt",ios::in);
   fin>>optDir;
+
   string pdflatexcmd;
   if(optDir==".") pdflatexcmd="pdflatex "+NmOfOptTexFl;
   else pdflatexcmd="pdflatex -output-directory="+optDir+" "+NmOfOptTexFl;
