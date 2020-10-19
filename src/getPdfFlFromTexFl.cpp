799550c1abaab40c1a2046f5b83d9a64c9664a0f
Xing-Yu Zhou <xing-yu.zhou@desy.de>
2020-10-19 19:07:11 +0900

    Update src/getPdfFlFromTexFl.cpp in order to write the pdf file to the directory of the tex source file, in the cases where the tex source file is not in the current working directory.

--- src/getPdfFlFromTexFl.cpp
+++ src/getPdfFlFromTexFl.cpp
@@ -6,7 +6,16 @@ void topoana::getPdfFlFromTexFl()
   if(m_anaTasksForSigIds=="T"&&m_vPid_compDcyBrP.size()==0&&m_vCompIncDcyBr.size()==0&&m_vCompIRADcyBr.size()==0&&m_compAnaOfDcyTrs==false&&m_compAnaOfDcyIFSts==false) return;
 
   string NmOfOptTexFl=m_comNmOfOptFls+".tex";
-  string pdflatexcmd="pdflatex "+NmOfOptTexFl;
+  //string pdflatexcmd="pdflatex "+NmOfOptTexFl;
+  // The following eight lines of code is used to replace the statement above, in order to write the pdf file to the directory of the tex source file, in the cases where the tex source file is not in the current working directory.
+  string optDir;
+  string dirnamecmd="dirname "+NmOfOptTexFl+" > dirname.txt";
+  system(dirnamecmd.c_str());
+  ifstream fin("dirname.txt",ios::in);
+  fin>>optDir;
+  string pdflatexcmd;
+  if(optDir==".") pdflatexcmd="pdflatex "+NmOfOptTexFl;
+  else pdflatexcmd="pdflatex -output-directory="+optDir+" "+NmOfOptTexFl;
   system(pdflatexcmd.c_str());
   system(pdflatexcmd.c_str());
   system(pdflatexcmd.c_str());
@@ -15,6 +24,6 @@ void topoana::getPdfFlFromTexFl()
   string NmOfOptLogFl=m_comNmOfOptFls+".log";
   string NmOfOptLotFl=m_comNmOfOptFls+".lot";
   string NmOfOptOutFl=m_comNmOfOptFls+".out";
-  string rmcmd="rm "+NmOfOptAuxFl+" "+NmOfOptLogFl+" "+NmOfOptLotFl+" "+NmOfOptOutFl;
+  string rmcmd="rm dirname.txt "+NmOfOptAuxFl+" "+NmOfOptLogFl+" "+NmOfOptLotFl+" "+NmOfOptOutFl;
   system(rmcmd.c_str());
 }
