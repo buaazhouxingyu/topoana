35eb5fc0b80a31bf8c4dfc2e55173f0601ae5965
Xing-Yu Zhou <xing-yu.zhou@desy.de>
2021-01-17 16:17:27 +0900

    Update the version number from 2.8.4 to 2.8.5

--- include/topoana.h
+++ include/topoana.h
@@ -274,8 +274,8 @@ class topoana
   public:
     topoana()
     {
-      m_pkgPath="/workfs/bes/zhouxy/topoana-02-08-04/";
-      m_verNum="v2.8.4";
+      m_pkgPath="/workfs/bes/zhouxy/topoana-02-08-05/";
+      m_verNum="v2.8.5";
       m_cardFlNm0=m_pkgPath+"share/underlying_topoana.card";
 
       m_nmsOfIptRootFls.clear();
